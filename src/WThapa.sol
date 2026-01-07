418955b8456516723a27766aeb140f13c0ca6dce
Pragyan Thapa <pragyanthapa44@gmail.com>
2026-01-08 00:00:47 +0530

    Initial Bridge Contract written

--- src/WThapa.sol
+++ src/WThapa.sol
@@ -1,7 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 contract WThapa is ERC20, Ownable{
-    constructor()ERC20("Wrapped Thapa","WTHP"){
+    constructor()ERC20("Wrapped Thapa","WTHP")Ownable(msg.sender){
 
     }
     function mint(address to, uint256 amount)public isOwner{
